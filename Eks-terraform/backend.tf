224eda4ab138a8dc796579fe9b4029c7b0242a28
Lokesh9192 <150279524+Lokesh9192@users.noreply.github.com>
2025-01-05 19:48:38 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbooktwotier" # Replace with your actual S3 bucket name
-    key    = "EKS-Ajay/terraform.tfstate"
+    bucket = "lokeshreddybucket" # Replace with your actual S3 bucket name
+    key    = "Jenkins-Reddy/terraform.tfstate"
     region = "ap-south-1"
   }
 }
